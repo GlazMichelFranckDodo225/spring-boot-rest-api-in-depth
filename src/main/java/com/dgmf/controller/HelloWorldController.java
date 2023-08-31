01a908608f8a0359fea30039cff4f670522ffe5c
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-08-31 06:20:42 +0200

    Simple Spring Boot REST API - @GetMapping("/hello-world")

---
+++ src/main/java/com/dgmf/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.dgmf.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // Http Get Request
+    @GetMapping("/hello-world") // http://localhost:8080/hello-world
+    public String helloWorld() {
+        return "Hello World !";
+    }
+}

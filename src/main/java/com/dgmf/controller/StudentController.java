face65e1dcb4d4db788541bbaa3d674bc109775d
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-09-06 16:36:45 +0200

    Create Spring Boot REST API returns List

--- src/main/java/com/dgmf/controller/StudentController.java
+++ src/main/java/com/dgmf/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.dgmf.dto.StudentDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     @GetMapping("/student") // http://localhost:8080/student
@@ -15,5 +18,17 @@ public class StudentController {
                 .build();
 
         return studentDTO;
+    }
+
+    @GetMapping("/students") // http://localhost:8080/students
+    public List<StudentDTO> getStudents() {
+        List<StudentDTO> studentDTOS = new ArrayList<>();
+        studentDTOS.add(new StudentDTO(2L, "Alex", "Gomez"));
+        studentDTOS.add(new StudentDTO(3L, "Guy", "Stewart"));
+        studentDTOS.add(new StudentDTO(4L, "Francis", "Pedro"));
+        studentDTOS.add(new StudentDTO(5L, "Nolan", "Howard"));
+        studentDTOS.add(new StudentDTO(6L, "Edmond", "Lopez"));
+
+        return studentDTOS;
     }
 }

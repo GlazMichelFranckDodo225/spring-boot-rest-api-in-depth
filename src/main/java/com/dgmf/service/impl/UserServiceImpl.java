df68e3a325f2c0a8c6d67dd3c885f4447d393e9b
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-11 08:25:28 +0100

    Using MapStruct Library: Step 3 - Using Mapper and Test CRUD REST API's

--- src/main/java/com/dgmf/service/impl/UserServiceImpl.java
+++ src/main/java/com/dgmf/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.dgmf.service.impl;
 
 import com.dgmf.entity.User;
+import com.dgmf.mapper.AutoUserMapper;
 import com.dgmf.mapper.UserMapper;
 import com.dgmf.repository.UserRepository;
 import com.dgmf.service.UserService;
@@ -21,15 +22,23 @@ public class UserServiceImpl implements UserService {
     public UserDto createUser(UserDto userDTO) {
         // Convert UserDto into User JPA Entity
         // User user = UserMapper.mapUserDtoToUser(userDTO);
+
         // Using ModelMapper API
-        User user = modelMapper.map(userDTO, User.class);
+        // User user = modelMapper.map(userDTO, User.class);
+
+        // Using MapStruct API
+        User user = AutoUserMapper.MAPPER.mapToUser(userDTO);
 
         User savedUser = userRepository.save(user);
 
         // Convert User JPA Entity into UserDto
         // UserDto savedUserDto = UserMapper.mapUserToUserDto(savedUser);
+
         // Using ModelMapper API
-        UserDto savedUserDto = modelMapper.map(savedUser, UserDto.class);
+        // UserDto savedUserDto = modelMapper.map(savedUser, UserDto.class);
+
+        // Using MapStruct API
+        UserDto savedUserDto = AutoUserMapper.MAPPER.mapToUserDto(savedUser);
 
         return savedUserDto;
     }
@@ -39,8 +48,12 @@ public class UserServiceImpl implements UserService {
         User user = userRepository.findById(userId).get();
 
         // UserDto userDtoResponse = UserMapper.mapUserToUserDto(user);
+
         // Using ModelMapper API
-        UserDto userDtoResponse = modelMapper.map(user, UserDto.class);
+        // UserDto userDtoResponse = modelMapper.map(user, UserDto.class);
+
+        // Using MapStruct API
+        UserDto userDtoResponse = AutoUserMapper.MAPPER.mapToUserDto(user);
 
         return userDtoResponse;
     }
@@ -49,33 +62,24 @@ public class UserServiceImpl implements UserService {
     public List<UserDto> getAllUsers() {
         List<User> users = userRepository.findAll();
 
-        /*List<UserDto> userDtos = new ArrayList<>();
-
-        for (User user: users) {
-            UserDto userDtoResponse = UserDto.builder()
-                    .firstName(user.getFirstName())
-                    .lastName(user.getLastName())
-                    .email(user.getEmail())
-                    .build();
-
-            userDtos.add(userDtoResponse);
-        }*/
-
         /*return users.stream()
                 .map(UserMapper::mapUserToUserDto)
                 .collect(Collectors.toList());*/
+
         // Using ModelMapper API
-        return users.stream()
+        /*return users.stream()
                 .map(user -> modelMapper.map(user, UserDto.class))
+                .collect(Collectors.toList());*/
+
+        // Using MapStruct API
+        return users.stream()
+                .map(user -> AutoUserMapper.MAPPER.mapToUserDto(user))
                 .collect(Collectors.toList());
     }
 
     @Override
-    public UserDto updateUser(
-            UserDto UserDto,
-            Long UserDtoId) {
-        User existingUser = userRepository
-                .findById(UserDtoId).get();
+    public UserDto updateUser(UserDto UserDto, Long UserDtoId) {
+        User existingUser = userRepository.findById(UserDtoId).get();
 
         existingUser.setFirstName(UserDto.getFirstName());
         existingUser.setLastName(UserDto.getLastName());
@@ -84,8 +88,12 @@ public class UserServiceImpl implements UserService {
         User updatedUser = userRepository.save(existingUser);
 
         // return UserMapper.mapUserToUserDto(updatedUser);
+
         // Using ModelMapper API
-        return modelMapper.map(updatedUser, UserDto.class);
+        // return modelMapper.map(updatedUser, UserDto.class);
+
+        // Using MapStruct API
+        return AutoUserMapper.MAPPER.mapToUserDto(updatedUser);
     }
 
     @Override

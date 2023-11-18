dabda7057b1ad146450a5d37e10f3268a2738f51
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-18 15:48:10 +0100

    Handle a specific Exception from the UserController and sending a custom Error Response to the Client

---
+++ src/main/java/com/dgmf/exception/ErrorDetails.java
@@ -0,0 +1,16 @@
+package com.dgmf.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String path;
+    private String errorCode;
+}
--- src/main/java/com/dgmf/web/controller/UserController.java
+++ src/main/java/com/dgmf/web/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.dgmf.web.controller;
 
+import com.dgmf.exception.ErrorDetails;
+import com.dgmf.exception.ResourceNotFoundException;
 import com.dgmf.service.UserService;
 import com.dgmf.web.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -62,5 +66,23 @@ public class UserController {
         return ResponseEntity
                 .ok("User with id : "
                         + UserDtoId + " Deleted Successfully.");
+    }
+
+    // Build Exception Handler
+    // @ExceptionHandler() ==> To handle specific Exception and
+    // sending a custom Response to the Client
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(
+            ResourceNotFoundException resourceNotFoundException,
+            WebRequest webRequest
+    ) {
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .message(resourceNotFoundException.getMessage())
+                .path(webRequest.getDescription(false))
+                .errorCode("USER_NOT_FOUND")
+                .build();
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 }

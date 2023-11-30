cac47d811c6fa485934b57efea44a58e109be7d4
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-30 04:18:16 +0100

    Add Validation Annotations to UserDto

--- src/main/java/com/dgmf/web/dto/UserDto.java
+++ src/main/java/com/dgmf/web/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.dgmf.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,7 +10,11 @@ import lombok.NoArgsConstructor;
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class UserDto {
     private Long id;
+    @NotEmpty // Value is not "Null" or "Empty"
     private String firstName;
+    @NotEmpty
     private String lastName;
+    @NotEmpty
+    @Email // Email should be a Valid Email Address
     private String email;
 }

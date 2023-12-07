8a7ede29a9236dbff8d221a77755b6019fe62dbe
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-12-07 08:54:49 +0100

    Generate REST API Documentation using SpringDoc OpenAPI

--- src/main/java/com/dgmf/web/controller/UserController.java
+++ src/main/java/com/dgmf/web/controller/UserController.java
@@ -66,7 +66,7 @@ public class UserController {
         userService.deleteUserById(UserDtoId);
 
         return ResponseEntity
-                .ok("User with id : "
+                .ok("User with id "
                         + UserDtoId + " Deleted Successfully.");
     }
 

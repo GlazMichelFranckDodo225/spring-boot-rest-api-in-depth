0a6d5897a530cd1d9d0ca77194f9892b1a7a8847
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-12-08 11:48:22 +0100

    Customizing UserDto Model Information with Swagger Annotations

--- src/main/java/com/dgmf/web/dto/UserDto.java
+++ src/main/java/com/dgmf/web/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.dgmf.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -7,13 +8,26 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// Swagger Model Documentation
+@Schema(
+        description = "UserDto Model Information"
+)
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class UserDto {
     private Long id;
+    @Schema(
+            description = "User First Name"
+    )
     @NotEmpty(message = "User First Name should not be Null or Empty")
     private String firstName;
+    @Schema(
+            description = "User Last Name"
+    )
     @NotEmpty(message = "User Last Name should not be Null or Empty")
     private String lastName;
+    @Schema(
+            description = "User Email Address"
+    )
     @NotEmpty(message = "User Email should not be Null or Empty")
     @Email(message = "User Email should be a Valid Email Address")
     private String email;
